a319ff3100d1a36344160d800a301e52dcb3379f
techvfxking <jobs.biplab.sharma@gmail.com>
2023-03-05 14:49:40 +0530

    Revamp 2.0

--- index.js
+++ index.js
@@ -2,12 +2,26 @@
 let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
+let loader = document.getElementById("loading");
+
+let displayLoading = () => {
+  loader.classList.add("display");
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 5000);
+}
+
+let hideLoading = () => {
+  loader.classList.remove("display");
+}
 
 let getMovie = () => {
+  displayLoading();
   let movieName = movieNameRef.value;
-  let url = `https://www.omdbapi.com/?t=${movieName}&apikey=11531d44`;
+  let url = `https://www.omdbapi.com/?t=${movieName}&apikey=11531d44&plot=full`;
   if (movieName.length <= 0) {
-    result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
+    hideLoading();
+    result.innerHTML = `<h3 class="msg">Please enter a movie name</h3>`;
   }
   else {
     fetch(url)
@@ -21,8 +35,10 @@ let getMovie = () => {
                 <div>
                     <h2>${data.Title}</h2>
                     <div class="rating">
-                        <img src="./star-icon.svg">
+                        <img src="./star-icon.svg">Ratings:
                         <h4>${data.imdbRating}</h4>
+                        <img src="./imdb-3.png">Votes:
+                        <h4>${data.imdbVotes}</h4>
                     </div>
                     <div class="details">
                         <span>${data.Rated}</span>
@@ -38,17 +54,36 @@ let getMovie = () => {
             <p>${data.Plot}</p>
             <h3>Cast:</h3>
             <p>${data.Actors}</p>
-            
+            <h3>Awards:</h3>
+            <p>${data.Awards}</p>
+            <h3>BoxOffice: <span style="font-size: 0.9em;
+            font-weight: 300;
+            line-height: 1.8em;
+            text-align: justify;
+            color: #a0a0a0;">${data.BoxOffice == null ?"Not Found":data.BoxOffice}</span></h3>
+            <h3>Writer:</h3>
+            <p>${data.Writer}</p>                 
         `;
-        }
+        hideLoading();
+       }
         else {
+          hideLoading();
           result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
         }
       })
       .catch(() => {
+        hideLoading();
         result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
       });
   }
 };
+movieNameRef.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    getMovie();
+  }
+});
+
 searchBtn.addEventListener("click", getMovie);
 window.addEventListener("load", getMovie);
+
